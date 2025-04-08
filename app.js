997f7224da3f6b97adc2c7a0f55d4d4ed6790a71
Karthick-FSD <sai.karthick0895@gmail.com>
2025-04-08 21:05:30 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1", {id:"heading", class:"heading1"}, "Hello World from REACT");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
