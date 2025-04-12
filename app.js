6868470e8e2c214e6179be44a6541b6a26a4320c
Karthick-FSD <sai.karthick0895@gmail.com>
2025-04-12 16:15:24 +0530

    Episode 2

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const heading = React.createElement("h1", {id:"heading", class:"heading1"}, "Hello World from REACT");
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement("h1", {id:"heading", className:"heading1"}, "This is Namaste REACT 🩷");
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
